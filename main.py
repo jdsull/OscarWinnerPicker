32267e7fa42f501bae95f7b1212d371f4898c4eb
jdsull <142814119+jdsull@users.noreply.github.com>
2023-09-05 17:40:13 +0000

    modified:   main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+'''
+A program to rank actors and actresses by number of awards won.
+'''
+
+import pandas as pd
+
+def readFile():
+    df = pd.read_csv('data/oscar_data.csv')
+    print(df.toString())
+
+readFile()
 
